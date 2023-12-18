248155b04eae4732bf1efded8fff90cc6e2dbeee
Yu-Chen <0913toby@gmail.com>
2023-12-18 21:14:48 +0800

    上傳檔案

--- EMS_test/EMS.py
+++ EMS_test/EMS.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+kWh = 1000 # 1kWh = 1000度電
+
 class TOU:
       def __init__(self):
             # TOU參數
@@ -8,21 +10,22 @@ class TOU:
             self.non_summer_peak_price = 4.48
             self.non_summer_off_peak_price = 1.78
 
-      def get_tou(self, month, day, hour):  # 取得當前時間電價
-            summer_month = 6 <= month <= 9
-            weekday = 1 <= day <= 5
+      def get_tou(self):  # 取得當前時間電價
+            current_time = datetime.now()
+            summer_month = 6 <= current_time.month <= 9
+            weekday = 1 <= (current_time.weekday() + 1) <= 5
 
             if summer_month:
-                  if weekday and 9 <= hour <= 24:
+                  if weekday and 9 <= current_time.hour <= 24:
                         return "尖峰", self.summer_peak_price
-                  elif not weekday and (6 <= hour <= 11 or 14 <= hour <= 24):
+                  elif not weekday and (6 <= current_time.hour <= 11 or 14 <= current_time.hour <= 24):
                         return "尖峰", self.non_summer_peak_price
                   else:
                         return "離峰", self.non_summer_off_peak_price  # 非夏月離峰時間
             else:
-                  if weekday and (6 <= hour <= 11 or 14 <= hour <= 24):
+                  if weekday and (6 <= current_time.hour <= 11 or 14 <= current_time.hour <= 24):
                         return "尖峰", self.non_summer_peak_price
-                  elif not weekday and (0 <= hour <= 6 or 11 <= hour <= 14):
+                  elif not weekday and (0 <= current_time.hour <= 6 or 11 <= current_time.hour <= 14):
                         return "離峰", self.non_summer_off_peak_price
                   else:
                         return "離峰", self.summer_off_peak_price  # 非夏月離峰時間
@@ -51,6 +54,7 @@ class TOU:
 
             return rate
 
+
 class Grid:
       def __init__(self):
             # 電網參數
@@ -66,21 +70,22 @@ class Grid:
                   print("電網提供電量：", self.max_output_power)
                   return self.max_output_power
 
+
 class ESS:
       def __init__(self):
             # 儲能系統參數
-            self.battery_max_capacity = 200*1000  
+            self.battery_max_capacity = 1000 * kWh
             self.battery_min_capacity = 0
             self.current_battery = 0
 
             self.now_soc = 0
             self.target_soc = 0
 
-            self.max_output_power = 100   # 儲能系統最大輸出功率
+            self.max_output_power = 200*1000   # 儲能系統最大輸出功率
             self.current_output_power = 0
             self.output_power_time = 0
 
-            self.max_input_power = 100    # 儲能系統最大輸入功率  
+            self.max_input_power = 200*1000    # 儲能系統最大輸入功率  
             self.current_input_power = 0
             self.input_power_time = 0
 
@@ -89,14 +94,13 @@ class ESS:
 
             self.ess_state = 0  # 0:不動作 1:充電 2:放電
 
-
-      def charge_battery(self, amount):      # 儲能系統充電
-            if self.current_battery + amount <= self.battery_max_capacity:
+      def charge_battery(self):      # 儲能系統充電
+            if self.current_battery + self.max_input_power <= self.battery_max_capacity:
                   self.ess_state = 1
-                  if amount / (self.end_charge_time - self.start_charge_time) > self.max_input_power:
+                  if self.max_input_power / (self.end_charge_time - self.start_charge_time) > self.max_input_power:
                         charge_power = self.max_input_power
                   else:
-                        charge_power = amount / (self.end_charge_time - self.start_charge_time)
+                        charge_power = self.max_input_power / (self.end_charge_time - self.start_charge_time)
                   self.current_battery += charge_power
                   print(f"儲能系統充電，目前電量：{self.current_battery}")
                   return (self.current_battery, charge_power)
@@ -106,14 +110,21 @@ class ESS:
                   return (self.current_battery)
       
       def provide_power(self, load_demand):   # 儲能放電
-            if load_demand > self.current_battery:
-                  self.ess_state = 2
-                  print("需求超過儲能系統目前電量，只能提供：", self.current_battery)
-                  return self.current_battery
+            if self.current_battery == 0:
+                  self.ess_state = 0
+                  print("儲能系統電量為0，無法提供電量。")
+                  return 0
             else:
-                  self.ess_state = 2
-                  print("儲能系統滿足需求，目前電量為：" , self.current_battery, "，提供電量為：", load_demand)
-                  return load_demand
+                  if load_demand > self.current_battery:
+                        self.ess_state = 2
+                        print("需求超過儲能系統目前電量，只能提供：", self.current_battery)
+                        self.current_battery -= load_demand
+                        return self.current_battery
+                  else:
+                        self.ess_state = 2
+                        print("儲能系統滿足需求，目前電量為：" , self.current_battery, "，提供電量為：", load_demand)
+                        self.current_battery -= load_demand
+                        return load_demand
 
       def get_current_battery(self):
             return self.current_battery
@@ -121,11 +132,18 @@ class ESS:
       def get_ess_state(self):
             return self.ess_state
       
+      def change_ess_state(self, state):
+            if state == 'charge':
+                  self.ess_state = 1
+            elif state == 'discharge':
+                  self.ess_state = 2
+            elif state == 'idle':
+                  self.ess_state = 0
+      
+
 class GC:
       def __init__(self):
             # GC參數
-            self.now_tou_price = 0
-
             self.if_ess_provide_power = False
             self.if_PV_provide_power = False
 
@@ -142,39 +160,70 @@ class GC:
             self.ess_state = self.ess.get_ess_state()
             self.ess_current_battery = self.ess.get_current_battery()
 
-            if self.ess_state == 0:
-                  print("儲能系統不動作。")
-                  self.ess_provide_power = 0
-                  self.if_ess_provide_power = False
-            elif self.ess_state == 1:
-                  print("儲能系統充電。")
-                  self.ess_provide_power = self.ess.charge_battery(load_demand)
-                  self.if_ess_provide_power = False
-            elif self.ess_state == 2:
-                  print("儲能系統放電。")
-                  self.ess_provide_power = self.ess.provide_power(load_demand)
+            self.tou_price = self.tou.get_tou()[0]
+
+            if self.tou_price == "尖峰":
+                  print("目前是尖峰時段，由儲能供電。")
+                  self.ess.change_ess_state('discharge')
                   self.if_ess_provide_power = True
+                  self.ess_provide_power = self.ess.provide_power(load_demand)
+                  return (self.ess_provide_power)
+            
+            elif self.tou_price == "離峰":
+                  if load_demand > 0:
+                        print("目前是離峰時段，由電網供電。")
+                        self.ess.change_ess_state('idle')
+                        self.if_ess_provide_power = False
+                        self.ess_provide_power = 0
+                        self.if_grid_provide_power = True
+                        self.grid_provide_power = self.grid.provide_power(load_demand)
+                        return (self.grid_provide_power)
+                  else:
+                        if self.charging_scheduler():
+                              print("目前是離峰時段，且電動車無充電需求，儲能充電。")
+                              self.ess.change_ess_state('charge')
+                              self.if_ess_provide_power = False
+                              self.ess_provide_power = 0
+                              self.if_grid_provide_power = True
+                              self.grid_provide_power = self.ess.charge_battery()
+                              return (self.grid_provide_power)
+                        else:
+                              print("目前是離峰時段，但電動車可能有充電需求，儲能不充電。")
+                              self.ess.change_ess_state('idle')
+                              self.if_ess_provide_power = False
+                              self.ess_provide_power = 0
 
-            if self.if_ess_provide_power == True:
-                  self.grid_provide_power = self.grid.provide_power(load_demand - self.ess_provide_power)
+                  
+      def charging_scheduler(self):
+            self.summer_off_peak_start_hour = 6
+            self.summer_off_peak_end_hour = 9
+            self.non_summer_off_peak_start_hour = 11
+            self.non_summer_off_peak_end_hour = 14
+
+            summer_month = 6 <= datetime.now().month <= 9
+
+            if summer_month:
+                  if self.summer_off_peak_start_hour <= datetime.now().hour <= self.summer_off_peak_end_hour:
+                        return True
+                  else:
+                        return False
             else:
-                  self.grid_provide_power = self.grid.provide_power(load_demand)
-
-            print("電網提供電量：", self.grid_provide_power)
-            print("儲能系統提供電量：", self.ess_provide_power)
-            return (self.grid_provide_power, self.ess_provide_power)
-
+                  if self.non_summer_off_peak_start_hour <= datetime.now().hour <= self.non_summer_off_peak_end_hour:
+                        return True
+                  else:
+                        return False
 
 
 
 class EVCS:
       def __init__(self):
             self.number = 0
+            self.connected_evs = []
 
-            self.max_output_power = 100
+            self.max_output_power = 100*1000
             self.current_output_power = 0
             self.min_output_power = 0
-            self.suitable_charging_power = 50
+            self.suitable_charging_power = 0
 
             self.charge_start_time = 0
             self.charge_end_time = 0
@@ -186,28 +235,34 @@ class EVCS:
             self.start_SOC = 0
             self.now_SOC = 0
 
+      def add_ev(self, ev, pile_number):
+            # 新進一輛車，連接到充電站
+            ev.pile_number = pile_number
+            self.connected_evs.append(ev)
+
+            self.total_power_needed = sum(ev.calculate_charge_power() for ev in self.connected_evs)
+
       def charge(self):
-            if self.gun_usage_amount == 1:
-                  print("目前是一支槍在使用。")
-                  self.max_charging_power = self.max_charging_power / 2
-                  return 1
-            elif self.gun_usage_amount == 2:
-                  print("目前是兩支槍在使用。")
-                  return 2
+            if ev.calculate_charge_power() <= self.max_output_power:
+                  self.current_output_power = ev.calculate_charge_power()
             else:
-                  print("目前沒有槍在使用。")
-                  return False
+                  self.current_output_power = self.max_output_power
+
+            for ev in self.connected_evs:
+                  ev.charge(self.current_output_power)
+
+            print(f"充電站{self.number}充電功率：{self.current_output_power}，當前SOC：{self.now_SOC}")
             
       def connect_to_ess(self, ess):
             self.ess = ess
 
 
 class EV:
-      def __init__(self, number, msx_capacity, target_SOC, now_SOC, power_limit, charge_end_time):
+      def __init__(self, number, target_SOC, now_SOC, power_limit, charge_end_time):
             # 電動車參數
             self.number = number
 
-            self.battery_max_capacity = msx_capacity
+            self.battery_max_capacity = 300 * kWh
 
             self.target_SOC = target_SOC
             self.now_SOC = now_SOC
--- EMS_test/MEStest.py
+++ EMS_test/MEStest.py
@@ -19,17 +19,20 @@ class EVCS:
     def __init__(self, number, ess):
         # 充電站初始設定
         self.number = number
-        self.connected_evs = []
+        self.connected_evs = []  # 使用列表存儲車輛
         self.ess = ess
 
-    def add_ev(self, ev, pile_number):
+    def add_ev(self, ev):
         # 新進一輛車，連接到充電站
-        ev.pile_number = pile_number  # 設定車輛的充電樁編號
         self.connected_evs.append(ev)
+
+    def update_connected_evs(self, evs):
+        # 動態更新連接的車輛列表
+        self.connected_evs = evs
 
     def calculate_total_power_needed(self):
         # 計算所有連接車輛的總充電功率需求
-        total_power_needed = sum(ev.calculate_charge_power() for ev in self.connected_evs)
+        total_power_needed = sum(ev.update_charge_status() for ev in self.connected_evs)
         return total_power_needed
 
     def inform_ess(self):
@@ -38,9 +41,9 @@ class EVCS:
         provided_power = self.ess.provide_power(total_power_needed)
         return provided_power
     
-    def receive_ev_status(self, ev_number, ev_soc, charge_power, pile_number):
+    def receive_ev_status(self, ev_number, ev_soc, charge_power):
         # 充電站接收電動車的狀態
-        print(f"EVCS {self.number} - Pile {pile_number} - Connected EV: {ev_number} - SOC: {ev_soc:.2f} - Charge Power: {charge_power:.2f}")
+        print(f"EVCS {self.number} - Connected EV: {ev_number} - SOC: {ev_soc:.2f} - Charge Power: {charge_power:.2f}")
 
 class EV:
     def __init__(self, number, max_capacity, now_soc, target_soc, power_limit, charge_duration):
@@ -52,41 +55,49 @@ class EV:
         self.power_limit = power_limit
         self.charge_duration = charge_duration
         self.charge_pi = 0
-        self.pile_number = None  # 初始時未指定充電樁編號
 
-    def calculate_charge_power(self):
-        # 計算充電功率（平均分散）
-        for elapsed_time in range(self.charge_duration):
-            remaining_time = max(0, self.charge_duration - elapsed_time)
-            if remaining_time > 0:
-                charge_soc_per_second = (self.target_soc - self.now_soc) / remaining_time
-                charge_power_per_second = charge_soc_per_second * self.max_capacity
-                charge_power = min(charge_power_per_second, self.power_limit)
-                self.now_soc += charge_power / self.max_capacity
+    def calculate_charge_power(self, elapsed_time):
+        remaining_time = max(0, self.charge_duration - elapsed_time)
+        if remaining_time > 0:
+            charge_soc_per_second = (self.target_soc - self.now_soc) / remaining_time
+            charge_power_per_second = charge_soc_per_second * self.max_capacity
+            charge_power = min(charge_power_per_second, self.power_limit)
+            return charge_power
+        else:
+            return 0
 
-                # 提供當前 SOC 和充電功率給充電站
-                evcs_1.receive_ev_status(self.number, self.now_soc, charge_power, self.pile_number)
+    def update_charge_status(self, elapsed_time):
+        time.sleep(1)
+        charge_power = self.calculate_charge_power(elapsed_time)
+        self.now_soc += charge_power / self.max_capacity
 
-                # 顯示當前狀態（可以根據需要進行調整）
-                # print(f"EV {self.number} - SOC: {self.now_soc:.2f} - Charge Power: {charge_power:.2f}")
-                time.sleep(1)  # 模擬每秒更新一次電量和當前 SOC
+        # 提供當前 SOC 和充電功率給充電站
+        evcs_1.receive_ev_status(self.number, self.now_soc, charge_power)
 
-    def update_charge_status(self):
-        # 計算充電功率，這個方法用於初始化時呼叫
-        self.calculate_charge_power()
+        # 顯示當前狀態（可以根據需要進行調整）
+        # print(f"EV {self.number} - SOC: {self.now_soc:.2f} - Charge Power: {charge_power:.2f}")
+        return charge_power
 
-# 創建儲能系統、充電站、和電動車
+# 創建儲能系統和充電站
 ess = ESS()
 evcs_1 = EVCS("1-1", ess)
 
-# 車輛進入充電站
+# 創建一輛車輛，可以根據需要動態增減
 ev1 = EV(1, 100, 0.2, 0.8, 50, 4)
-ev2 = EV(2, 120, 0.25, 0.9, 60, 3)
-
-# 車輛加入充電站，並指定充電樁編號
-evcs_1.add_ev(ev1, "1-1")
-evcs_1.add_ev(ev2, "1-2")
+evcs_1.add_ev(ev1)
 
 # 模擬充電過程
-ev1.update_charge_status()
-ev2.update_charge_status()
+for elapsed_time in range(ev1.charge_duration):
+    ev1.update_charge_status(elapsed_time)
+
+# 創建另一輛車輛，可以根據需要動態增減
+ev2 = EV(2, 120, 0.25, 0.9, 60, 3)
+evcs_1.add_ev(ev2)
+
+# 更新充電站連接的車輛列表
+evcs_1.update_connected_evs([ev1, ev2])
+
+# 模擬充電過程
+for elapsed_time in range(max(ev1.charge_duration, ev2.charge_duration)):
+    ev1.update_charge_status(elapsed_time)
+    ev2.update_charge_status(elapsed_time)
